b81d55ff05945e4e1ee133a75887d69b6af2e1fb
Nik Startsev <nikstar1235@gmail.com>
2023-12-13 14:41:12 +0300

    3. LazyInitializationexception [#331987 #465436]

--- db/scripts/004_ddl_create_priorities_table.sql
+++ db/scripts/004_ddl_create_priorities_table.sql
@@ -10,6 +10,3 @@ INSERT INTO priorities (name, position) VALUES ('normal', 2);
 ALTER TABLE tasks ADD COLUMN priority_id int REFERENCES priorities(id);
 
 UPDATE tasks SET priority_id = (SELECT id FROM priorities WHERE name = 'urgently');
-
-select * from priorities
-select * from tasks
